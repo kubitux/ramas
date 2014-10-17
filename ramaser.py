2db6b5102081633f449b8145155402e086179e36
tyler <outmonmon@gmail.com>
2014-10-17 09:51:12 +0400

    updateredis fonction2

--- ramaser.py
+++ ramaser.py
@@ -19,32 +19,42 @@ def parsing(mot_french="manger"):
 	r = requests.post("http://www.creole.org/cgi-bin/dico/dictionnaire_creole.pl", data=payload)
 	soup = bs.BeautifulSoup(r.text)
 	trad = False
-
-	for elem in soup.find_all('font'):
-		 if "Francais" in str(elem):
-			 trad = elem
-			 break
-	if not trad:
+	
+	try:
+		elem = soup.find_all('font')[6]
+	except:
 		return {}
-	print trad
-	trad2= str(trad).split("<br/>")
 
+	if "Francais" in str(elem):
+		trad = elem
+	else:
+		return {}
+
+	trad2= str(trad).split("<br>")
 	trad3=[]
 
 	for elem in trad2:
-		if ("Francais" in elem) or ("Créole" in elem):
+		if ("Francais" in elem) or ("Cr\xc3\xa9ole" in elem):
 			trad3.append(elem.split('White"> ')[1])
-	print "trad :"
-
-
+	if trad3==[]:
+		return{}
 	#enleve espace et saut de ligne		
 	trad3 = [elem.replace("\r\n","") for elem in trad3]
 
 	dico={}
+
 	for mot in trad3:
 		index = trad3.index(mot)
 		if index%2==0:
-			dico[mot.replace(" ","").replace("1-","").replace("2-","").replace("3-","").replace("4-","").replace("5-","")]=trad3[index+1]
+			try:	
+				dico[mot.replace(" ","").replace("1-","").replace("2-","").replace("3-","").replace("4-","").replace("5-","")]=trad3[index+1]
+			except:
+				print "ERROR :" + mot_french
+				print "trad2 :"
+				print trad2
+				print "trad3 :"
+				print trad3
+				print "index="+str(index)
 
 	return dico	
 
@@ -54,11 +64,9 @@ if __name__ == "__main__":
 	french=open('liste_francais.txt','r')
 	for mot in french.readlines():
 		dico.update(parsing(mot))
-		print dico
 
-
-	with open('dico.json', 'w') as f:
+	with open(DICO_CREOLE, 'w') as f:
 		json.dump(dico, f, indent=4)
 
 	f.close()
-		
+
